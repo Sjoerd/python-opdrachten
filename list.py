d99bdaabe5541be11b6b28c80a02d54e82c986c1
Sjoerd <info@sjoerdk.nl>
2020-02-10 12:02:01 +0100

    :)

--- list.py
+++ list.py
@@ -1,7 +1,17 @@
+# http://github.com/sjoerrdd/python-opdrachten
 # Lijst met lijsten
 # Rooster maken, 1-100. 1-10, 11-20
 
-getallen = [list(range(i, i+10)) for i in range(1,100,10)]
+getallen = [list(range(i, i+10)) for i in range(1,100,10)] # list() i.p.v. [], anders worden de haakjes ook geprint.
 
 for getal in getallen:
   print(" ".join(map(str, getal)))
+
+
+# Lange versie:
+# getallen = []
+# for i in range(1, 100, 10):
+#   getallen.append(list(range(i, i + 10))
+
+# for getal in getallen:
+#   print(" ".join(map(str, getal)))

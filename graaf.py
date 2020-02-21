2ecaaaa948732b3a1ff51f897f76c7fea101d453
Sjoerd <Sjoerrdd@users.noreply.github.com>
2020-02-21 14:25:17 +0100

    Create graaf.py

---
+++ graaf.py
@@ -0,0 +1,25 @@
+ongerichteGraaf = [{'a', 'b'}, {'b', 'c'}, {'b', 'd'}, {'c', 'a'}]
+gerichteGraaf = [('a', 'b'), ('b', 'c'), ('b', 'd'), ('c', 'a')]
+
+
+# Schrijf een functie die een ongerichte graaf als parameter heeft en de verzameling knopen (vertices) retourneert.
+def ongerichteVerzamelingKnopen(graaf):
+  return 1
+
+# Schrijf een functie die een gerichte graaf als parameter heeft en de verzameling knopen (vertices) retourneert.
+def gerichteVerzamelingKnopen(graaf):
+  return 1
+
+# Schrijf een functie die een boolean waarde retourneert die aangeeft of de (on)gerichte graaf een multigraaf is of niet.
+def isMultigraaf(graaf):
+  if True:
+    return True
+  else:
+    return False
+
+
+gerichteVerzamelingKnopen(gerichteGraaf)
+ongerichteVerzamelingKnopen(ongerichteGraaf)
+
+
+
